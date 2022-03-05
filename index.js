15a79a1c29328ea8654cdb59f2cdff5a63441cb1
dedrie p <dedrie@dedries-MacBook-Air.local>
2022-03-05 00:40:16 -0500

    added additional team members

--- index.js
+++ index.js
@@ -1,10 +1,61 @@
 const fs = require('fs')
 var inquirer = require('inquirer')
 
-const managerQuestions = 
-{
 
-        type: 'input',
-        name: 'Manager Name',
-        message: 'What is your Team Managers Name?'
+
+// questions to generate manager info
+const managerQuestions =
+    [
+        {
+
+            type: 'input',
+            name: 'Manager',
+            message: 'What is your Team Managers Name? (Required)',
+            validate: manager => {
+                if (manager) {
+                    return true;
+                } else {
+                    console.log("Please enter your Team Manager's name?")
+                    return false
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'phone',
+            message: 'What is the Team Managers phone number?',
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the Team Managers email?',
+        },
+        
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: 'What is the Team Managers Employee ID?',
+        },
+    ]
+
+// function to generate team member info
+    const teamMembers = [
+
+        {
+
+            type: 'list',
+            name: 'addTeam',
+            message: 'Would you like to add additional Team Members',
+            choices: ["Engineer", "Intern", "None"]
+        },
+    
+
+    ]
+
+function init() {
+    inquirer.prompt(managerQuestions)
 }
+
+init()
--- src/homepage.js
+++ src/homepage.js
@@ -1,4 +1,4 @@
 
-
-
 //module.exports = homepageTemplate
+
+// function to generate html
